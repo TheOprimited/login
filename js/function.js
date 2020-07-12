7cfeea2f762a9d3228f026822fdc3f3bb3641edf
Thales TheOprimited <aviario070@gmail.com>
2020-07-12 20:23:42 -0300

    first commit

---
+++ js/function.js
@@ -0,0 +1,21 @@
+function validar(){
+    let nm =document.getElementById("nm").value
+    let ps = document.getElementById("ps").value
+    let um = document.getElementById("um").value
+    let pr = document.getElementById("pr").value
+    if (nm == ""){
+        alert("falta nome")
+    }
+    if(ps == ""){
+    alert("precisa do peso")
+    
+    if(um == ""){
+        alert("necessita unidade de medida")
+        }
+    if(pr ==""){
+        alert("qual o preço?")
+    }
+
+    }
+    
+}
